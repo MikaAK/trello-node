b891800972df160efe0c59c38aeac58b29351a91
MikaAK <mika@kalathil.me>
2018-04-28 18:55:02 -0700

    add updage/create card and createCardLabel

--- src/Trello.js
+++ src/Trello.js
@@ -1,5 +1,23 @@
 import {createRequest} from './helpers'
 
+const attachCardsToLists = (Trello) => (lists) => {
+  var promises = lists.map(list => Trello.getListCards(list.id))
+
+  return Promise.all([lists].concat(promises))
+    .then(function(cards) {
+      var lists = cards.shift()
+
+      for (var i = 0, k = lists.length; i < k; i++) {
+        if (!lists[i].cards)
+          lists[i].cards = []
+
+        lists[i].cards = cards[i]
+      }
+
+      return lists
+    })
+}
+
 export default class Trello {
   static getAuthorizeUrl(token) {
     return `https://trello.com/1/connect?key=${token}&name=Edvisor&response_type=token&expiration=never&scope=read,write`
@@ -59,24 +77,22 @@ export default class Trello {
 
   getBoardLists(boardId) {
     return this.get(`/boards/${boardId}/lists`)
-      .then(this.attachCardsToLists.bind(this))
+      .then(attachCardsToLists(this))
   }
 
-  attachCardsToLists(lists) {
-    var promises = lists.map(list => this.getListCards(list.id))
+  createCard(data) {
+    return this.post('/cards', data)
+  }
 
-    return Promise.all([lists].concat(promises))
-      .then(function(cards) {
-        var lists = cards.shift()
+  createCardLabel(cardId, labelData) {
+    return this.post(`/cards/${cardId}/labels`, labelData)
+  }
 
-        for (var i = 0, k = lists.length; i < k; i++) {
-          if (!lists[i].cards)
-            lists[i].cards = []
+  updateCard(cardId, updateData) {
+    return this.put(`/cards/${cardId}`, updateData)
+  }
 
-          lists[i].cards = cards[i]
-        }
-
-        return lists
-      })
+  deleteCard(cardId, labelId) {
+    return `/cards/${cardId}/idLabels/${labelId}`
   }
 }
